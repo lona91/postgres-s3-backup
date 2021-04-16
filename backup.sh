2e64b942988e490e053808f3409e2ffdfd4a2a5b
Luca Lonati <info@atlas-software.it>
2021-04-16 23:06:05 +0200

    changed filename

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 db_host="postgresql://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}"
 current_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-file="backup_${current_date}.gz"
+file="pg_${DATABASE_NAME}_${current_date}.gz"
 
 pg_dump $db_host | gzip > $file
 
